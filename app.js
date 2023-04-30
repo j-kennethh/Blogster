0cab4eb08e13725dcfba1e9534d398218f6ef3ea
John Kenneth <johnkennethagpaoa@gmail.com>
2023-04-30 22:47:00 +0800

    set up get request to home route

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-
+app.get("/", function (req, res) {
+    res.render("home");
+});
 
 
 app.listen(3000, function() {
-  console.log("server running on port 3000");
+    console.log("server running on port 3000");
 });
